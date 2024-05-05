b604e8dfe8e5e7365d0c96e7072fe6b740338eba
Jeremy Perez <jperezhua@unsa.edu.pe>
2024-05-05 12:47:17 -0500

    Realizamos el segundo commit añadiendo el texto bienvenido al curso

--- fase-01/HelloWorld/src/helloworld/HelloWorld.java
+++ fase-01/HelloWorld/src/helloworld/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!");
+        System.out.println("Bienvenido al curso");
+
     }
     
 }
